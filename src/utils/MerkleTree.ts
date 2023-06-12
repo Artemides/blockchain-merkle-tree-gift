5d7a6fdd56271d0c26ec5d0c4d7c54b2e74b3135
Artemides <edmpulasky@gmail.com>
2023-06-12 12:28:10 -0500

    set up proy and create mrkle tree utils

---
+++ src/utils/MerkleTree.ts
@@ -0,0 +1,74 @@
+import { keccak256 } from "ethereum-cryptography/keccak";
+import { bytesToHex } from "ethereum-cryptography/utils";
+import { MerkleProof } from "./verifyProof";
+
+class MerkleTree {
+  leaves: Uint8Array[];
+  concat: (left: Uint8Array, right: Uint8Array) => Uint8Array;
+  constructor(leaves: string[]) {
+    this.leaves = leaves.map(Buffer.from).map(keccak256);
+    this.concat = (left: Uint8Array, right: Uint8Array) =>
+      keccak256(Buffer.concat([left, right]));
+  }
+
+  getRoot() {
+    return bytesToHex(this._getRoot(this.leaves));
+  }
+
+  getProof(
+    index: number,
+    layer = this.leaves,
+    proof: MerkleProof[] = []
+  ): MerkleProof[] {
+    if (layer.length === 1) {
+      return proof;
+    }
+
+    const newLayer = [];
+
+    for (let i = 0; i < layer.length; i += 2) {
+      const left = layer[i];
+      const right = layer[i + 1];
+
+      if (!right) {
+        newLayer.push(left);
+      } else {
+        newLayer.push(this.concat(left, right));
+
+        if (i === index || i === index - 1) {
+          let isLeft = !(index % 2);
+          proof.push({
+            data: isLeft ? bytesToHex(right) : bytesToHex(left),
+            left: !isLeft,
+          });
+        }
+      }
+    }
+
+    return this.getProof(Math.floor(index / 2), newLayer, proof);
+  }
+
+  // private function
+  _getRoot(leaves = this.leaves): Uint8Array {
+    if (leaves.length === 1) {
+      return leaves[0];
+    }
+
+    const layer = [];
+
+    for (let i = 0; i < leaves.length; i += 2) {
+      const left = leaves[i];
+      const right = leaves[i + 1];
+
+      if (right) {
+        layer.push(this.concat(left, right));
+      } else {
+        layer.push(left);
+      }
+    }
+
+    return this._getRoot(layer);
+  }
+}
+
+module.exports = MerkleTree;
---
+++ src/utils/verifyProof.ts
@@ -0,0 +1,30 @@
+import { keccak256 } from "ethereum-cryptography/keccak";
+import { hexToBytes, bytesToHex } from "ethereum-cryptography/utils";
+
+export type MerkleProof = {
+  data: Uint8Array | string;
+  left: boolean;
+};
+
+const concat = (left: Uint8Array, right: Uint8Array) =>
+  keccak256(Buffer.concat([left, right]));
+
+function verifyProof(proof: MerkleProof[], leaf: string, root: string) {
+  proof = proof.map(({ data, left }) => ({
+    left,
+    data: hexToBytes(data as string),
+  }));
+  let data = keccak256(Buffer.from(leaf));
+
+  for (let i = 0; i < proof.length; i++) {
+    if (proof[i].left) {
+      data = concat(proof[i].data as Uint8Array, data);
+    } else {
+      data = concat(data, proof[i].data as Uint8Array);
+    }
+  }
+
+  return bytesToHex(data) === root;
+}
+
+module.exports = verifyProof;
